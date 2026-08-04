a00d081259f93abf4f3c0b634914dabe8722b4cf
cachu94 <fgiacri.94@gmail.com>
2026-08-04 01:07:55 -0300

    avance 2

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./version-Tailwind/**/*.html",
+    "./version-Tailwind/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

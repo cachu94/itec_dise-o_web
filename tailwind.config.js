61ae08521f070e521bc36c21dada4bd0231fd70a
cachu94 <fgiacri.94@gmail.com>
2026-08-06 13:33:53 -0300

    Avance Hero 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./version-Tailwind/**/*.html",
-    "./version-Tailwind/**/*.js",
+    "./version-tailwind/**/*.html",
+    "./version-tailwind/**/*.js",
   ],
   theme: {
     extend: {},
